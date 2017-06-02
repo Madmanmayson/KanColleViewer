9d82279b669c89e60016ea5ad7cdcc72e4f888dd
Madmanmayson <madmanmayson45@gmail.com>
2017-06-02 07:20:36 -0700

    View Model mostly complete
    Missing data handling from homeport ship list

--- source/Plugins/Calculator/Models/ExperienceCalculator.cs
+++ source/Plugins/Calculator/Models/ExperienceCalculator.cs
@@ -4,30 +4,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Grabacr07.KanColleWrapper;
+using Grabacr07.KanColleViewer.ViewModels.Catalogs;
+using Grabacr07.KanColleWrapper.Models;
+using System.Reactive.Subjects;
+using System.Reactive;
+using Livet;
 
 namespace Calculator.Models
 {
-    class ExperienceCalculator
+    class ExperienceCalculator : NotificationObject
     {
-        private int[] experienceTable;
-        private Dictionary<string, int> sortieExperienceTable;
-        public string[] battleResults = { "S", "A", "B", "C", "D", "E" };
+        private readonly Subject<Unit> updateSource = new Subject<Unit>();
 
-        private string selectedMap;
-        private int remainingExperience;
-        private bool isFlagship;
-        private bool isMVP;
-        private int remainingBattles;
+        private int[] experienceTable;
+
+        private IEnumerable<string> sortieList;
+        private Dictionary<string, int> sortieExperienceTable;
+
+        public string[] battleResults = { "S", "A", "B", "C", "D", "E" };
+        private int sortieExperience;
+        private int currentLevel;
+        private int currentExperience;
+
+        #region CurrentShip
+
+        public Ship CurrentShip
+        {
+            get { return this.CurrentShip; }
+            set
+            {
+                if(this.CurrentShip != value)
+                {
+                    this.CurrentShip = value;
+                    this.targetLevel = Math.Min(this.CurrentShip.Level + 1, 155);
+                    this.currentLevel = CurrentShip.Level;
+                    this.currentExperience = CurrentShip.Exp;
+                    this.UpdateExperience();
+                }
+            }
+        }
+
+        #endregion
+
+        #region TargetLevel
+
+        public int targetLevel
+        {
+            get { return this.targetLevel; }
+            set
+            {
+                if(this.targetLevel != value)
+                {
+                    this.targetLevel = value;
+                    this.target
+                }
+            }
+        }
+
+        #endregion
+
+        public string selectedMap;
+        public bool isFlagship;
+        public bool isMVP;
+        public int remainingBattles;
+        public int remainingExperience;
+        public string battleResult;
 
         public ExperienceCalculator()
         {
-            experienceTable = ExperienceTable.experienceByLevel;
-            sortieExperienceTable = SortieExperienceTable.SortieExperience;
+            this.experienceTable = ExperienceTable.experienceByLevel;
+            this.sortieExperienceTable = SortieExperienceTable.SortieExperience;
+            this.sortieList = sortieExperienceTable.Keys;
 
-            isFlagship = false;
-            isMVP = false;
+            this.CurrentShip = KanColleClient.Current.Homeport.Organization.Ships.Values.First();
 
+            this.isFlagship = true;
+            this.isMVP = true;
+
+            battleResult = battleResults.FirstOrDefault();
             selectedMap = sortieExperienceTable.Keys.FirstOrDefault();
+        }
+
+
+        public void UpdateExperience()
+        {
+            //taken from YunYun's calculator plugin
+            double multiplier = (this.isFlagship ? 1.5 : 1) * (this.isMVP ? 2 : 1) * (this.battleResult == "S" ? 1.2 : (this.battleResult == "C" ? 0.8 : (this.battleResult == "D" ? 0.7 : (this.battleResult == "E" ? 0.5 : 1))));
+
+            this.sortieExperience = (int)Math.Round(sortieExperienceTable[selectedMap] * multiplier);
+            this.remainingExperience = this.experienceTable[targetLevel] - this.currentExperience;
+            this.remainingBattles = (int)Math.Ceiling(this.remainingExperience / (double)this.sortieExperience);
         }
     }
 }
--- source/Plugins/Calculator/ViewModels/ToolViewModel.cs
+++ source/Plugins/Calculator/ViewModels/ToolViewModel.cs
@@ -5,14 +5,198 @@ using System.Text;
 using System.Threading.Tasks;
 using Livet;
 using Calculator;
+using Calculator.Models;
+using Livet.EventListeners;
+using Grabacr07.KanColleWrapper;
+using Grabacr07.KanColleWrapper.Models;
+using Grabacr07.KanColleViewer.ViewModels.Catalogs;
 
 namespace Calculator.ViewModels
 {
     class ToolViewModel : ViewModel
     {
+        public ExperienceCalculator calculator;
+
+        #region Ships
+
+        private IReadOnlyCollection<ShipViewModel> _Ships;
+
+        public IReadOnlyCollection<ShipViewModel> Ships
+        {
+            get { return this._Ships; }
+            set
+            {
+                if (this._Ships != value)
+                {
+                    this._Ships = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region CurrentShip
+
+        private Ship _CurrentShip;
+
+        public Ship CurrentShip
+        {
+            get { return this._CurrentShip; }
+            set
+            {
+                if (this._CurrentShip != value)
+                {
+                    this._CurrentShip = value;
+                    if (value != null)
+                    {
+                        this.calculator.CurrentShip = this._CurrentShip;
+                        this.RaisePropertyChanged();
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region SelectedResult
+
+        private string _SelectedResult;
+
+        public string SelectedResult
+        {
+            get { return this._SelectedResult; }
+            set
+            {
+                if (this._SelectedResult != value)
+                {
+                    this._SelectedResult = value;
+                    this.calculator.battleResult = _SelectedResult;
+                    this.RaisePropertyChanged();
+                    this.calculator.UpdateExperience();
+                }
+            }
+        }
+
+        #endregion
+
+        #region IsFlagship
+
+        private bool _IsFlagship;
+
+        public bool IsFlagship
+        {
+            get { return this._IsFlagship; }
+            set
+            {
+                if (this._IsFlagship != value)
+                {
+                    this._IsFlagship = value;
+                    this.RaisePropertyChanged();
+                    this.calculator.isFlagship = _IsFlagship;
+                    this.calculator.UpdateExperience();
+                }
+            }
+        }
+
+        #endregion
+
+        #region IsMVP
+
+        private bool _IsMVP;
+
+        public bool IsMVP
+        {
+            get { return this._IsMVP; }
+            set
+            {
+                if (this._IsMVP != value)
+                {
+                    this._IsMVP = value;
+                    this.RaisePropertyChanged();
+                    this.calculator.isMVP = _IsMVP;
+                    this.calculator.UpdateExperience();
+                }
+            }
+        }
+
+        #endregion
+
+        #region TargetLevel
+
+        private int _TargetLevel;
+
+        public int TargetLevel
+        {
+            get { return this._TargetLevel; }
+            set
+            {
+                if (this._TargetLevel != value && value >= 1 && value <= 150)
+                {
+                    this._TargetLevel = value;
+                    this.RaisePropertyChanged();
+                    this.calculator.UpdateExperience();
+                }
+            }
+        }
+
+        #endregion
+
+        #region RemainingExperience
+
+        private int _RemianingExperience;
+
+        public int RemainingExpereince
+        {
+            get { return this._RemianingExperience; }
+            set
+            {
+                if(this._RemianingExperience != value)
+                {
+                    this._RemianingExperience = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region RemainingBattles
+
+        private int _RemainingBattles;
+
+        public int RemainingBattles
+        {
+            get { return this._RemainingBattles; }
+            set
+            {
+                if (this._RemainingBattles != value)
+                {
+                    this._RemainingBattles = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
         public ToolViewModel(Calculator plugin)
         {
+            calculator = new ExperienceCalculator();
 
+            this.CompositeDisposable.Add(new PropertyChangedEventListener(this.calculator)
+            {
+                {
+                    () => this.calculator.remainingBattles,
+                    (_, __) => this.RaisePropertyChanged(() => this.RemainingExpereince)
+                },
+                {
+                    () => this.calculator.remainingBattles,
+                    (_, __) => this.RaisePropertyChanged(() => this.RemainingBattles)
+                },
+            });
         }
+
+
     }
 }
