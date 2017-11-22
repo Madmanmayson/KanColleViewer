101c53fabed73fb65c00018a6a1ebd0f3b5834fa
Madmanmayson <madmanmayson45@gmail.com>
2017-11-21 22:07:12 -0800

    Release 4.2.11.1

--- source/Grabacr07.KanColleViewer/Properties/AssemblyInfo.cs
+++ source/Grabacr07.KanColleViewer/Properties/AssemblyInfo.cs
@@ -21,4 +21,4 @@ using System.Windows;
 	ResourceDictionaryLocation.None,
 	ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("4.2.11.0")]
+[assembly: AssemblyVersion("4.2.11.1")]
