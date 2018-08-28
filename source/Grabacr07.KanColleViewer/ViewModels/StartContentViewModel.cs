21f0521f9a880971f7f17742db3718521a54a719
Unknown <madmanmayson45@gmail.com>
2018-08-27 22:38:37 -0700

    Readd Region Cookie Button (Whoops)

--- source/Grabacr07.KanColleViewer/ViewModels/StartContentViewModel.cs
+++ source/Grabacr07.KanColleViewer/ViewModels/StartContentViewModel.cs
@@ -19,6 +19,51 @@ namespace Grabacr07.KanColleViewer.ViewModels
 			set => GeneralSettings.ClearCacheOnNextStartup.Value = value;
 		}
 
+		#region CanSetRegionCookie 変更通知プロパティ 
+
+		private bool _CanSetRegionCookie = true;
+
+		public bool CanSetRegionCookie
+		{
+			get { return this._CanSetRegionCookie; }
+			set
+			{
+				if (this._CanSetRegionCookie != value)
+				{
+					this._CanSetRegionCookie = value;
+					this.RaisePropertyChanged();
+				}
+			}
+		}
+
+		#endregion
+
+		#region SetRegionCookieButtonContent 変更通知プロパティ 
+		private string _SetRegionCookieButtonContent = Properties.Resources.StartContent_SetRegionCookieButton;
+
+		public string SetRegionCookieButtonContent
+		{
+			get { return this._SetRegionCookieButtonContent; }
+			set
+			{
+				if (this._SetRegionCookieButtonContent != value)
+				{
+					this._SetRegionCookieButtonContent = value;
+					this.RaisePropertyChanged();
+				}
+			}
+		}
+		#endregion
+
+		public void SetRegionCookie()
+		{
+			if (!(WindowService.Current.MainWindow is KanColleWindowViewModel)) return;
+			var navigator = ((KanColleWindowViewModel)WindowService.Current.MainWindow).Navigator;
+
+			navigator.CookieNavigate();
+			this.SetRegionCookieButtonContent = Properties.Resources.StartContent_SetRegionCookieMessage;
+			this.CanSetRegionCookie = false;
+		}
 
 		public StartContentViewModel(NavigatorViewModel navigator)
 		{
